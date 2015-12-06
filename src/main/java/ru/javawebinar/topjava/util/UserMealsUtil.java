877955f6f864af3b1616a77ba512eba2f98f87f5
VitalyMCMXC <vitaly.moskalik@gmail.com>
2015-12-06 20:20:57 +0300

    commit

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -5,7 +5,6 @@ import ru.javawebinar.topjava.model.UserMealWithExceed;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.Month;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -17,63 +16,45 @@ import java.util.stream.Collectors;
 public class UserMealsUtil {
 
     private static int caloriesPerDay = 2000;
-    private static  List<UserMeal> mealList = Arrays.asList(
-            new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
-            new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
-            new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
-            new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
-            new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
-            new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
-    );
-
-    public static int getCaloriesPerDay() {
-        return caloriesPerDay;
+    private static  List<UserMeal> mealList = new ArrayList<>();
+    static {
+        mealList.add(new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500));
+        mealList.add(new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000));
+        mealList.add(new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500));
+        mealList.add(new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000));
+        mealList.add(new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500));
+        mealList.add(new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510));
     }
 
-    public static void setCaloriesPerDay(int caloriesPerDay) {
-        UserMealsUtil.caloriesPerDay = caloriesPerDay;
-    }
-
-    public static List<UserMealWithExceed> getMealsWithExceed(){
+    public static Map<LocalDateTime, List<UserMealWithExceed>> getFilteredMeals(LocalDateTime startTime, LocalDateTime endTime) {
         Map<LocalDate, Integer> caloriesSumByDate = mealList.stream()
                 .collect(Collectors.groupingBy(um -> um.getDateTime().toLocalDate(),
                         Collectors.summingInt(UserMeal::getCalories)));
 
-        return mealList.stream()
-                .map(um -> new UserMealWithExceed(
-                        um.getDateTime(),
-                        um.getDescription(),
-                        um.getCalories(),
-                        caloriesSumByDate.get(um.getDateTime().toLocalDate()) > caloriesPerDay))
-                .collect(Collectors.toList());
-    }
-
-    public static List<UserMealWithExceed> getFilteredMealsWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        Map<LocalDate, Integer> caloriesSumByDate = mealList.stream().collect(Collectors.groupingBy(um -> um.getDateTime().toLocalDate(), Collectors.summingInt(UserMeal::getCalories)));
-
-        return mealList.stream()
-                .filter(um -> TimeUtil.isBetween(um.getDateTime().toLocalTime(), startTime, endTime))
+        Map<LocalDateTime, List<UserMealWithExceed>> map =  mealList.stream()
+                .filter(um -> TimeUtil.isBetweenGlobal(um.getDateTime(), startTime, endTime))
                 .map(um -> new UserMealWithExceed(um.getDateTime(), um.getDescription(), um.getCalories(),
                         caloriesSumByDate.get(um.getDateTime().toLocalDate()) > caloriesPerDay))
-                .collect(Collectors.toList());
+                .collect(Collectors.groupingBy(UserMealWithExceed::getDateTime));
+
+        return new TreeMap<>(map);
     }
 
-    public static List<UserMealWithExceed> getFilteredMealsWithExceededByCycle(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+    public static void addMeal(LocalDateTime dateTime, String description, int calories){
+        mealList.add(new UserMeal(dateTime, description, calories));
+    }
 
-        Map<LocalDate, Integer> caloriesSumPerDate = new HashMap<>();
-        for (UserMeal meal : mealList) {
-            LocalDate mealDate = meal.getDateTime().toLocalDate();
-            caloriesSumPerDate.put(mealDate, caloriesSumPerDate.merge(mealDate, 0, (oldValue, newValue) -> oldValue + newValue));
-        }
-
-        List<UserMealWithExceed> mealExceeded = new ArrayList<>();
-        for (UserMeal meal : mealList) {
-            LocalDateTime dateTime = meal.getDateTime();
-            if (TimeUtil.isBetween(dateTime.toLocalTime(), startTime, endTime)) {
-                mealExceeded.add(new UserMealWithExceed(dateTime, meal.getDescription(), meal.getCalories(),
-                        caloriesSumPerDate.get(dateTime.toLocalDate()) > caloriesPerDay));
+    public static void deleteMeal(LocalDateTime dateTime, String description){
+        for (int i = 0; i < mealList.size(); i++) {
+            UserMeal um = mealList.get(i);
+            if (um.getDateTime().equals(dateTime) && um.getDescription().equals(description)) {
+                mealList.remove(i);
+                break;
             }
         }
-        return mealExceeded;
+    }
+
+    public static UserMeal newUserMeal(LocalDateTime dateTime, String description, int calories){
+        return new UserMeal(dateTime, description, calories);
     }
 }
