df3d1b8a0672a30993bbc4db5794ceee21b60fc8
VitalyMCMXC <vitaly.moskalik@gmail.com>
2016-01-05 02:39:37 +0300

    HW02

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -10,6 +10,5 @@ package ru.javawebinar.topjava;
 public class Main {
     public static void main(String[] args) {
         System.out.format("Hello Topjava Enterprise!");
-        System.out.println("Hi, World!");
     }
 }
