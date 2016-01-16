9b4bc3e01bf082afe5e699c44ba525ba04f28690
VitalyMCMXC <vitaly.moskalik@gmail.com>
2016-01-17 02:44:35 +0300

    HW03

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -23,3 +23,11 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+  id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  datetime TIMESTAMP NOT NULL,
+  description VARCHAR NOT NULL,
+  calories INTEGER NOT NULL
+);
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -11,3 +12,11 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories ) VALUES
+  (localtimestamp, 'Завтрак', 500),
+  (localtimestamp, 'Обед', 1000),
+  (localtimestamp, 'Ужин', 500),
+  (make_timestamp(2015, 12, 31, 09, 00, 00), 'Завтрак', 500),
+  (make_timestamp(2015, 12, 31, 14, 00, 00), 'Обед', 1000),
+  (make_timestamp(2015, 12, 31, 23, 00, 00), 'Ужин', 900);
